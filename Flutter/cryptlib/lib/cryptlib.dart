3856629db2806216163058ef2c2489a48327c2c4
93cgutierrez <34190481+93cgutierrez@users.noreply.github.com>
2022-01-31 22:30:52 -0500

    Update null safety dart
    Change String to String? (null)

--- Flutter/cryptlib/lib/cryptlib.dart
+++ Flutter/cryptlib/lib/cryptlib.dart
@@ -10,21 +10,21 @@ class Cryptlib {
     return version;
   }
 
-  static Future<String> getEncryptedString(String key, String text) async {
+  static Future<String?> getEncryptedString(String key, String text) async {
     Map<String, dynamic> args = <String, dynamic>{};
     args.putIfAbsent("key", () => key);
     args.putIfAbsent("text", () => text);
-    final String encrypted =
+    final String? encrypted =
         await _channel.invokeMethod('getEncryptedString', args);
     return encrypted;
   }
 
-  static Future<String> getDecryptedString(
+  static Future<String?> getDecryptedString(
       String key, String encryptedText) async {
     Map<String, dynamic> args = <String, dynamic>{};
     args.putIfAbsent("key", () => key);
     args.putIfAbsent("encryptedText", () => encryptedText);
-    final String encrypted =
+    final String? encrypted =
         await _channel.invokeMethod('getDecryptedString', args);
     return encrypted;
   }
